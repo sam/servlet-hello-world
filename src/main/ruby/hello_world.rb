f74d3e7c60379a2b77b32813d31afe83054823b4
Sam Smoot <ssmoot@gmail.com>
2012-08-29 14:30:46 -0500

    Using a Ruby Symbol where a Java String is required by the method signature.

--- src/main/ruby/hello_world.rb
+++ src/main/ruby/hello_world.rb
@@ -5,7 +5,17 @@ class HelloWorld < HttpServlet
   def doGet(request, response)
     writer = response.writer
     writer.println "Hello World!"
-    if id = request.get_parameter("id")
+    # NOTE: A Symbol is used here, which matches the 
+    # Signature of HttpServletRequest#getParameter(String name)
+    # So Symbols, which are interned strings in Ruby, must be
+    # treated as Strings by JRuby. Meaning you can freely use
+    # Symbols on Java Methods requiring a String Argument.
+    # Long story short, if you're accessing a Java Map, then
+    # there's no need for something like the common Ruby idiom
+    # of a HashWithIndifferentAccess since both Ruby Symbols
+    # and Ruby Strings will be dispatched appropriately to
+    # Java methods requiring a String in the signature.
+    if id = request.get_parameter(:id)
       writer.println "The value of \"id\" is #{id.inspect}"
     end
   end
